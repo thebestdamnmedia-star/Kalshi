c2ff9babbc80f5ccca4043f1f5725ecf952f11f6
thebestdamnmedia-star <thebestdamnmedia@gmail.com>
2026-05-10 02:11:51 -0400

    Update logger.js

--- api/logger.js
+++ api/logger.js
@@ -44,7 +44,7 @@ module.exports = async function handler(req, res) {
   
   try {
     const end = Math.floor(Date.now() / 1000);
-    const start = end - 5 * 60;
+    const start = end - 15 * 60;
     const fmt = ts => new Date(ts * 1000).toISOString().slice(0, 10);
     const url = 'https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/minute/' + fmt(start) + '/' + fmt(end) + '?adjusted=true&sort=desc&limit=1&apiKey=' + POLYGON_KEY;
     const r = await fetch(url);
